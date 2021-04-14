04a90fae183ebb77bce23ca0ca4adad5f6b40e4b
Jerome Boyer <boyerje@us.ibm.com>
2021-04-13 18:31:19 -0700

    First drop

---
+++ src/test/java/ibm/garage/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ibm.garage;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends InventoryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
